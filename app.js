bb53140bfc8984e4552ecbac93bc610848b99456
Daniel Johnson <dsaved8291@gmail.com>
2022-05-10 19:12:37 +0000

    Update to rate limiter setup

--- app.js
+++ app.js
@@ -17,11 +17,13 @@ var io = require('socket.io')(server, { origins: "*" });
 
 
 // set up rate limiter: maximum of five requests per minute
-var RateLimit = require('express-rate-limit');
-var limiter = new RateLimit({
-    windowMs: 1 * 60 * 1000, // 1 minute
-    max: 5
-});
+var rateLimit = require('express-rate-limit');
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
 
 // apply rate limiter to all requests
 app.use(limiter);
